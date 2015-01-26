f190ff28e8affdb84072ab158a45b69a44df3654
Robert Pająk <pellared@hotmail.com>
2015-01-26 14:52:35 +0100

    Sample class with a unit test

---
+++ Game/Game/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace Game
+{
+    public class Game
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+}
---
+++ Game/Game/GameTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Game
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Add_values_returns_the_sum()
+        {
+            var game = new Game();
+            int result = game.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+    }
+}
