b7f727f25d99b1bd8cc1836f79367d2caeedcd3b
PellaredTraining <pellared@tlen.pl>
2015-02-03 10:27:45 +0100

    v 2

--- Game/Game/Game.cs
+++ Game/Game/Game.cs
@@ -5,7 +5,30 @@
 
         public string ConvertToRomanNumber(int i)
         {
-            return "I";
+            return ConvertBasicNumbers(i);
+        }
+
+        private  string ConvertBasicNumbers(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    return "I";
+                case 5:
+                    return "V";
+                case 10:
+                    return "X";
+                case 50:
+                    return "L";
+                case 100:
+                    return "C";
+                case 500:
+                    return "D";
+                case 1000:
+                    return "M";
+                default:
+                    throw new System.ApplicationException(string.Format("Unknown number to convert: '{0}'.", number));
+            }
         }
     }
 }
